4580cc11504b321554767e4281eb51a14c3dc22c
Paul Austin <paul@ni.com>
2014-09-09 15:43:53 -0500

    samples: An example framework for and RC file used by LaBVIEW

---
+++ sample-vis/SampleLVNativeMethod/Canvas2D/Canvas2D/Canvas2D.c
@@ -0,0 +1,56 @@
+//
+// Canvas2D.c
+//
+// This source for a sample DLL(windows) / sharedlibrary(linux) / framework(OSX)
+// That LabVIEW will load as part of loadind a custom .RC file
+//
+// These are just stubs. This is just an example.
+
+#import "Canvas2D.h"
+
+typedef void* Canvas2DContextRef;
+
+void BeginPath(Canvas2DContextRef context)
+{
+
+}
+
+void EndPath(Canvas2DContextRef context)
+{
+    
+}
+
+void StrokePath(Canvas2DContextRef context)
+{
+    
+}
+
+void FillPath(Canvas2DContextRef context)
+{
+    
+}
+
+void Save(Canvas2DContextRef context)
+{
+    
+}
+
+void Restore(Canvas2DContextRef context)
+{
+    
+}
+
+void MoveTo(Canvas2DContextRef context, double x, double y)
+{
+    
+}
+
+void LineTo(Canvas2DContextRef context, double x, double y)
+{
+    
+}
+
+void FillRect(Canvas2DContextRef context, double x, double y, double width, double height)
+{
+    
+}
---
+++ sample-vis/SampleLVNativeMethod/Canvas2D/Canvas2D/Canvas2D.h
@@ -0,0 +1,13 @@
+//
+//  Canvas2D.h
+//  Canvas2D
+//
+//  Created by Paul Austin on 9/9/14.
+//  Copyright (c) 2014 Vireo. All rights reserved.
+//
+
+//#import <Foundation/Foundation.h>
+
+//@interface Canvas2D : NSObject
+
+//@end
