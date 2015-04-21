a748bb9525ba184f24215b44707591377f053178
unknown <sschwahn@LVD-4B56HQ1.amer.corp.natinst.com>
2015-04-21 14:59:58 -0500

    updated http client

--- source/io/library_httpClient.js
+++ source/io/library_httpClient.js
@@ -1,74 +1,198 @@
+/*jshint browser: true, node: true*/
+var HttpClient = {
 
-var HttpClient = 
-    {
-        js_ni_httpClient_Get: function (url, len, response, done)
-        {
-            function get(urlString) {
-                return new Promise (function (resolve, reject) {
-                    var request = new XMLHttpRequest();
-                    request.open("GET", urlString);
+    // Make a header module
+    headers: (function () {
+        var headers = {};
 
-                    request.onload = function () {
-                       if (request.status == 200) {
-                            resolve(request.responseText)
-                        }
-                        else {
-                            reject(Error(request.statusText));
-                        }
-                    };
-
-                    request.onerror = function () {
-                        reject(Error("Network Error"));
-                    };
-
-                    request.send();
-                });
+        return {
+            // Adds a key/value pair to the headers or update it if it exists
+            addHeader: function (name, value) {
+                headers[name] = value;
+            },
+            
+            removeHeader: function(name) {
+                delete headers[name];
+            },
+            
+            // Gets all the headers as an object of key/value pairs.
+            getHeaders: function () {
+                return headers;
+            },
+            
+            // Request a specific value based on a given key
+            getHeader: function (name) {
+                return headers[name] || '';
+            },
+            
+            headerExists: function(name) {
+                return headers.hasOwnProperty(name);
             }
-
-            get(Pointer_stringify(url, len)).then(
-                function (responseText) {
-                    NationalInstruments.Vireo.dataWriteString(response, responseText, responseText.length);
-                    var doneText = 'done';
-                    NationalInstruments.Vireo.dataWriteString(done, doneText, doneText.length);
-                    //console.log("In JavaScript World responseText is " + responseText);
-                },
-                function(error) {
-                    var message = error.message;
-                    NationalInstruments.Vireo.dataWriteString(response, message, message.length);
-                    var doneText2 = 'done';
-                    NationalInstruments.Vireo.dataWriteString(done, doneText2, doneText2.length);
-                    console.log("We got an error in JavaScript World: " + message);
-                }
-            );
-        },      
-};
-
-/*
-{
-    js_ni_httpClient_Get: function (url, len, response) {
+        };
+    })(),
+    
+    loginManager: (function() {
+        var username = "";
+        var password = "";
+        var cookies = "";
+        
+        return {
+            addUsername : function(name) {
+                username = name;
+            },
+            addPassword : function(pass) {
+                password = pass;   
+            }
+            
+        };
+    })(),
+    
+    makeRequest: function(protocol, url, buffer, successCallback, errorCallback) {
         var request = new XMLHttpRequest();
-        request.open("get", Pointer_stringify(url, len), false); // For now we are making a synchronous call
-        request.send();
+        request.open(protocol, url);
 
-        //var requestResponse = request.responseText + '\0';
-        var requestResponse = request.responseText;
-        NationalInstruments.Vireo.dataWriteString(response, requestResponse, requestResponse.length);
-    }
-}*/
-/*{
-    js_ni_httpClient_Get: function (url, len, response) {
-        setTimeout(function () {
-            console.log("start");
-            var request = new XMLHttpRequest();
-            request.open("get", Pointer_stringify(url, len), false); // For now we are making a synchronous call
+        var error = function () {
+            errorCallback(request);
+        };
+        
+        // Set the headers
+        var allHeaders = this.headers.getHeaders();
+        for (var key in allHeaders) {
+            if (allHeaders.hasOwnProperty(key)) {
+                request.setRequestHeader(key, allHeaders[key]);
+            }
+        }
+
+        request.onreadystatechange = function () {
+            if (request.readyState === 4) {
+                if (request.status == 200) {
+                    successCallback(request);
+                } else {
+                    error();
+                }
+            }
+        };
+
+        request.onerror = function () {
+            error();
+        };
+
+        if (buffer === undefined) {
             request.send();
+        } else {
+            request.send(buffer);
+        }
+    },
 
-            //var requestResponse = request.responseText + '\0';
-            var requestResponse = request.responseText;
-            NationalInstruments.Vireo.dataWriteString(response, requestResponse, requestResponse.length);
-        }, 1000);
-       
+    js_ni_httpClient_Get: function (url) {
+        var successCallback = function(request) {
+            // Return Headers
+            console.log(request.getAllResponseHeaders());
+            
+            // Return body
+            console.log(request.responseText);
+        };
+        var errorCallback = function(request)
+        {
+            console.log(request.statusText);
+        };
+        
+        this.makeRequest('GET', url, undefined, successCallback, errorCallback);
+    },
+
+    js_ni_httpClient_Post: function (url, buffer) {
+
+        var successCallback = function(request) {
+           // Return Headers
+            console.log(request.getAllResponseHeaders());
+            
+            // Return body
+            console.log(request.responseText);
+        };
+        var errorCallback = function(request)
+        {
+            console.log(request.statusText);
+        };
+        
+        this.makeRequest('POST', url, buffer, successCallback, errorCallback);
+    },
+    
+    js_ni_httpClient_Put: function(url, buffer) {
+        var successCallback = function(request) {
+           // Return Headers
+            console.log(request.getAllResponseHeaders());
+            
+            // Return body
+            console.log(request.responseText);
+        };
+        var errorCallback = function(request)
+        {
+            console.log(request.statusText);
+        };
+        
+        this.makeRequest('PUT', url, buffer, successCallback, errorCallback);
+    },
+    
+    js_ni_httpClient_Delete: function(url) {
+         var successCallback = function(request) {
+           // Return Headers
+            console.log(request.getAllResponseHeaders());
+            
+            // Return body
+            console.log(request.responseText);
+        };
+        var errorCallback = function(request)
+        {
+            console.log(request.statusText);
+        };
+        
+        this.makeRequest('POST', url, undefined, successCallback, errorCallback);
+    },
+    
+    js_ni_httpClient_Head: function(url)
+    {
+        var successCallback = function(request) {
+           // Return Headers
+            console.log(request.getAllResponseHeaders());
+        };
+        var errorCallback = function(request)
+        {
+            console.log(request.statusText);
+        };
+        
+        this.makeRequest('HEAD', url, undefined, successCallback, errorCallback);
+    },
+
+    js_ni_httpClient_AddHeader: function (header, value) {
+        this.headers.addHeader(header, value);
+    },
+    
+    js_ni_httpClient_RemoveHeader : function(header) {
+        this.headers.removeHeader(header);   
+    },
+    
+    js_ni_httpClient_GetHeader : function(header) {
+        var headerValue = this.headers.getHeader(header);
+        console.log(headerValue);
+    },
+    
+    js_ni_httpClient_HeaderExists : function(header) {
+        var headerExists = this.headers.headerExists(header);
+        var headerValue = this.headers.getHeader(header);
+        
+        console.log(headerExists);
+        console.log(headerValue);
+    },
+    
+    js_ni_httpClient_ListHeaders : function() {
+        var allHeaders = this.headers.getHeaders();
+        var outputString = '';
+        for (var key in allHeaders) {
+            outputString += key + ': ' + allHeaders[key];
+            outputString += '\r\n';
+        }
+        
+        console.log(outputString);
     }
-}*/
-
-mergeInto(LibraryManager.library, HttpClient);
+        
+};
