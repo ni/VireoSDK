54d520f84011a94ffa5a93af5206b13969dde417
cglzaguilar <cagonzal@lvd-1ky0w12.amer.corp.natinst.com>
2015-04-23 15:18:54 -0500

    Added the following commands for HTTP Client support:
    Open Handle
    Close Handle
    Add Header
    Remove Header
    HeaderExist
    ListHeaders
    Get
    Head
    Put
    Delete
    Post

--- source/core/vireo.postamble.js
+++ source/core/vireo.postamble.js
@@ -14,6 +14,221 @@ Module.v_root =  Module.v_create(0);
 Module.v_userShell = Module.v_create(Module.v_root);
 Module.fpSync = function(fpId) {};
 
+HttpUser = function (userName, password) {
+    'use strict';
+    // Properties
+    this.userName = userName;
+    this.password = password;
+    this.headers = {};
+
+    // Methods
+    if (typeof this.HttpUserMethods !== 'function') {
+        var proto = HttpUser.prototype;
+        proto.HttpUserMethods = function () {
+        };
+
+        proto.setUserName = function (userName) {
+            this.userName = userName;
+        };
+
+        proto.getUserName = function () {
+            return this.userName;
+        };
+
+        proto.setPassword = function (password) {
+            this.password = password;
+        };
+
+        proto.getPassword = function () {
+            return this.password;
+        };
+
+        proto.addHeader = function (header, value) {
+            this.headers[header] = value;
+        };
+
+        proto.removeHeader = function (header) {
+            if (this.headers.hasOwnProperty(header))
+            {
+                delete this.headers[header];
+            }
+        };
+
+        proto.getHeaderValue = function (header) {
+            if (this.headers.hasOwnProperty(header))
+            {
+                return this.headers[header];
+            }
+            return '';
+        };
+
+        proto.headerExist = function (header) {
+            if (this.headers.hasOwnProperty(header))
+            {
+                return true;
+            }
+            return false;
+        };
+
+        proto.listHeaders = function () {
+            var outputHeaders = '';
+            for (var header in this.headers) {
+                outputHeaders += header + ': ' + this.headers[header];
+                outputHeaders += '\r\n';
+            }
+            return outputHeaders;
+        };
+
+        proto.getHeaders = function () {
+            return this.headers;
+        };
+    }
+};
+
+HttpUsers = function () {
+    'use strict';
+    // Properties
+    this.httpClients = {};
+
+    // Methods
+    if (typeof this.HttpUsersMethods !== 'function') {
+        var proto = HttpUsers.prototype;
+        proto.HttpUsersMethods = function () {
+        };
+
+        proto.add = function (userName, password) {
+            var httpUser = new HttpUser(userName, password);
+            var handle = Object.keys(this.httpClients).length + 1;
+            var strHandle = handle.toString();
+            this.httpClients[strHandle] = httpUser;
+            console.log ("httpUser created for: " + userName);
+            return handle;
+        };
+
+        proto.remove = function (handle) {
+            if (handle !== 0)
+            {
+                var strHandle = handle.toString();
+                if (this.httpClients.hasOwnProperty(strHandle))
+                {
+                    var user = this.httpClients[strHandle]
+                    console.log ("httpUser deleted for: " + user.getUserName());
+                    delete this.httpClients[strHandle];
+                }
+                else
+                {
+                    console.log ("httpUser error: Unknown handle (" + strHandle + ")");
+                    return -1;
+                }
+            }
+
+            return 0;
+        };
+
+        proto.get = function (handle) {
+            if (handle !== 0)
+            {
+                var strHandle = handle.toString();
+                if (this.httpClients.hasOwnProperty(strHandle))
+                {
+                    var user = this.httpClients[strHandle];
+                    console.log ("The following user was requested: " + user.getUserName());
+                    return user;
+                }
+            }
+            return null;
+        };
+
+        proto.addHeader = function (handle, header, value) {
+            if (handle !== 0)
+            {
+                var strHandle = handle.toString();
+                if (this.httpClients.hasOwnProperty(strHandle))
+                {
+                    var user = this.httpClients[strHandle];
+                    user.addHeader (header,value);
+                    console.log ("The following header was added: " + header + "(" + value + ")");
+                    return 0;
+                }
+                else
+                    return -1;
+            }
+            return -1;
+        };
+
+        proto.removeHeader = function (handle, header) {
+            if (handle !== 0)
+            {
+                var strHandle = handle.toString();
+                if (this.httpClients.hasOwnProperty(strHandle))
+                {
+                    var user = this.httpClients[strHandle];
+                    user.removeHeader (header);
+                    console.log ("The following header was removed: " + header);
+                    return 0;
+                }
+                else
+                    return -1;
+            }
+            return -1;
+        };
+
+        proto.getHeaderValue = function (handle, header) {
+            if (handle !== 0)
+            {
+                var strHandle = handle.toString();
+                if (this.httpClients.hasOwnProperty(strHandle))
+                {
+                    var user = this.httpClients[strHandle];
+                    var value = user.getHeaderValue (header);
+                    console.log ("The following value was returned:(" + value + ") for header: " + header);
+                    return value;
+                }
+                else
+                    return '';
+            }
+            return '';
+        };
+
+        proto.headerExist = function (handle, header) {
+            if (handle !== 0)
+            {
+                var strHandle = handle.toString();
+                if (this.httpClients.hasOwnProperty(strHandle))
+                {
+                    var user = this.httpClients[strHandle];
+                    var exist = user.headerExist (header);
+                    console.log ("The following value was returned:(" + exist + ") for headerExist: " + header);
+                    return exist;
+                }
+                else
+                    return false;
+            }
+            return false;
+        };
+
+        proto.listHeaders = function (handle) {
+            if (handle !== 0)
+            {
+                var strHandle = handle.toString();
+                if (this.httpClients.hasOwnProperty(strHandle))
+                {
+                    var user = this.httpClients[strHandle];
+                    var value = user.listHeaders ();
+                    console.log ("The following value was returned:(" + value + ") for listHeaders.");
+                    return value;
+                }
+                else
+                    return '';
+            }
+            return '';
+        };
+    }
+};
+
+var httpUsers = new HttpUsers ();
+httpUsers.add("",""); // Adding default user for empty handle (0)
+
 return {
     version: Module.cwrap('Vireo_Version', 'number', []),
 
@@ -43,7 +258,90 @@ return {
             Module.v_delete(Module.v_userShell);
             Module.v_userShell = Module.v_create(Module.v_root);
         },
-    core: Module
+    core: Module,
+    addHttpUser:
+        function (userName, password) {
+            return httpUsers.add (userName, password);
+        },
+    removeHttpUser:
+        function (handle) {
+            httpUsers.remove(handle);
+        },
+    getHttpUser:
+        function (handle) {
+            return httpUsers.get(handle);
+        },
+    addHeader:
+        function (handle, header, value) {
+            httpUsers.addHeader (handle, header, value);
+        },
+    removeHeader:
+        function (handle, header) {
+            return httpUsers.removeHeader (handle, header);
+        },
+    getHeaderValue:
+        function (handle, header, value) {
+            var valueText = httpUsers.getHeaderValue (handle, header);
+            NationalInstruments.Vireo.dataWriteString(value, valueText, valueText.length);
+            return 0;
+        },
+    headerExist:
+        function (handle, header) {
+            return httpUsers.headerExist (handle, header);
+        },
+    listHeaders:
+        function (handle, list) {
+            var listText = httpUsers.listHeaders (handle);
+            NationalInstruments.Vireo.dataWriteString(list, listText, listText.length);
+            return 0;
+        },
+    makeRequest: 
+        function (userHandle, protocol, url, timeOut, buffer, successCallback, errorCallback, onTimeOutCallback) {
+            var httpUser = this.getHttpUser(userHandle);
+
+            var request = new XMLHttpRequest();
+            request.open(protocol, url);
+
+            var error = function () {
+                errorCallback(request);
+            };
+
+            var onTimeOut = function () {
+                onTimeOutCallback(request);
+            }
+
+            // Set the headers
+            var allHeaders = httpUser.getHeaders();
+            for (var key in allHeaders) {
+                if (allHeaders.hasOwnProperty(key)) {
+                    request.setRequestHeader(key, allHeaders[key]);
+                }
+            }
+
+            request.onreadystatechange = function () {
+                if (request.readyState === 4) {
+                    if (request.status == 200) {
+                        successCallback(request);
+                    } else {
+                        error();
+                    }
+                }
+            };
+
+            request.onerror = function () {
+                error();
+            };
+
+            request.ontimeout = function () {
+                onTimeOut();
+            }
+
+            if (buffer === undefined) {
+                request.send();
+            } else {
+                request.send(buffer);
+            }
+        }
 };
 
 }());
--- source/io/library_httpClient.js
+++ source/io/library_httpClient.js
@@ -1,263 +1,161 @@
-var HttpClient = {
 
-    // Make a header module
-    headers: (function () {
-        var headers = {};
-
-        return {
-            // Adds a key/value pair to the headers or update it if it exists
-            addHeader: function (name, value) {
-                headers[name] = value;
-            },
-            
-            removeHeader: function(name) {
-                delete headers[name];
-            },
-            
-            // Gets all the headers as an object of key/value pairs.
-            getHeaders: function () {
-                return headers;
-            },
-            
-            // Request a specific value based on a given key
-            getHeader: function (name) {
-                return headers[name] || '';
-            },
-            
-            headerExists: function(name) {
-                return headers.hasOwnProperty(name);
-            },
-            
-            deleteHeaders: function() {
-                headers = {};   
-            }
-        };
-    })(),
-    
-    loginManager: (function() {
-        var username = "";
-        var password = "";
-        var cookies = {};
-        
-        return {
-            setUsername : function(name) {
-                username = name;
-            },
-            
-            setPassword : function(pass) {
-                password = pass;   
-            },
-            
-            addCookie : function(key, value) {
-                 cookies[key] = value;
-            },
-            
-            setCookies : function(cookieStr) {
-                var tokens = cookieStr.split(';');
-                for (var index in tokens) {
-                    var subTokens = tokens[index].split("=");
-                    if (subTokens.length === 2) {
-                        this.addCookie(subTokens[0].trim(), subTokens[1].trim());
-                    }
-                }
-            },
-        
-            getCookieString : function() {
-                var str = "";
-                for (var key in cookies) {
-                    str += key + "=" + cookies[key] + ";";
-                }
-                return str;
-            },
-            
-            clearData : function() {
-                username = "";
-                password = "";
-                
-                var str = this.getCookieString();
-                for (var key in cookies) {
-                    document.cookie = key + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';   
-                }
-                cookies = {};
-            },
-            
-            getRequest : function(protocol, url) {
-                var request = new XMLHttpRequest();
-                if (username !== "" && password !== "") {
-                    request.open(protocol, url, true, username, password);
-                }
-                else if (username !== "" && password === "") {
-                    request.open(protocol, url, true, username);
-                }
-                else {
-                    request.open(protocol, url, true);   
-                }
-                
-                if (Object.keys(cookies).length !== 0) {
-                    request.withCredentials = true;
-                    document.cookie += this.getCookieString();
-                }
-                return request;
-            }
-        };
-    })(),
-    
-    makeRequest: function(protocol, url, buffer, successCallback, errorCallback) {
-        var request = this.loginManager.getRequest(protocol, url);
-
-        var error = function () {
-            errorCallback(request);
-        };
-        
-        // Set the headers
-        var allHeaders = this.headers.getHeaders();
-        for (var key in allHeaders) {
-            if (allHeaders.hasOwnProperty(key)) {
-                request.setRequestHeader(key, allHeaders[key]);
-            }
-        }
-
-        request.onreadystatechange = function () {
-            if (request.readyState === 4) {
-                if (request.status == 200) {
-                    successCallback(request);
-                } else {
-                    error();
-                }
-            }
-        };
-
-        request.onerror = function () {
-            error();
-        };
-
-        if (buffer === undefined) {
-            request.send();
-        } else {
-            request.send(buffer);
-        }
-    },
-    
-    js_ni_httpClient_OpenHandle : function(cookies, username, password) {
-        this.loginManager.setCookies(cookies);
-        this.loginManager.setUsername(username);
-        this.loginManager.setPassword(password);
-    },
-    
-    js_ni_httpClient_CloseHandle : function(cookies, username, password) {
-        this.loginManager.clearData();
-        this.headers.deleteHeaders();
+var HttpClient =
+{
+    js_ni_httpClient_Open: function (cookieFile, cookieFileLength, userName, userNameLength, password, passwordLength, errorCode, verifyServer) {
+        return NationalInstruments.Vireo.addHttpUser(Pointer_stringify(userName, userNameLength), Pointer_stringify(password, passwordLength));
     },
 
-    js_ni_httpClient_Get: function (url) {
-        var successCallback = function(request) {
+    js_ni_httpClient_Close: function (userHandle) {
+        var user = NationalInstruments.Vireo.getHttpUser(userHandle);
+        return NationalInstruments.Vireo.removeHttpUser(userHandle);
+    },
+
+    js_ni_httpClient_AddHeader: function (userHandle, header, headerLength, value, valueLength) {
+        return NationalInstruments.Vireo.addHeader(userHandle, Pointer_stringify(header, headerLength), Pointer_stringify(value, valueLength));
+    },
+
+    js_ni_httpClient_RemoveHeader: function (userHandle, header, headerLength) {
+        return NationalInstruments.Vireo.removeHeader(userHandle, Pointer_stringify(header, headerLength));
+    },
+
+    js_ni_httpClient_GetHeader: function (userHandle, header, headerLength, value) {
+        return NationalInstruments.Vireo.getHeaderValue(userHandle, Pointer_stringify(header, headerLength), value);
+    },
+
+    js_ni_httpClient_HeaderExist: function (userHandle, header, headerLength) {
+        return NationalInstruments.Vireo.headerExist(userHandle, Pointer_stringify(header, headerLength));
+    },
+
+    js_ni_httpClient_ListHeaders: function (userHandle, list) {
+        return NationalInstruments.Vireo.listHeaders(userHandle, list);
+    },
+
+    js_ni_httpClient_Get: function (userHandle, url, urlLength, outputFile, outputFileLength, timeOut, headers, body, done) {
+        var doneText = 'done';
+        var successCallback = function (request) {
             // Return Headers
-            console.log(request.getAllResponseHeaders());
-            
+            var headersText = request.getAllResponseHeaders();
+            NationalInstruments.Vireo.dataWriteString(headers, headersText, headersText.length);
+
             // Return body
-            console.log(request.responseText);
+            var bodyText = request.responseText;
+            NationalInstruments.Vireo.dataWriteString(body, bodyText, bodyText.length);
+            NationalInstruments.Vireo.dataWriteString(done, doneText, doneText.length);
         };
-        var errorCallback = function(request)
-        {
+        var errorCallback = function (request) {
             console.log(request.statusText);
+            NationalInstruments.Vireo.dataWriteString(done, doneText, doneText.length);
         };
-        
-        this.makeRequest('GET', url, undefined, successCallback, errorCallback);
+        var timeOutCallback = function (request) {
+            console.log(request.statusText);
+            NationalInstruments.Vireo.dataWriteString(done, doneText, doneText.length);
+        };
+
+        NationalInstruments.Vireo.makeRequest(userHandle, 'GET', Pointer_stringify(url, urlLength), timeOut, undefined, successCallback, errorCallback, timeOutCallback);
+
+        return 0;
     },
 
-    js_ni_httpClient_Post: function (url, buffer) {
+    js_ni_httpClient_Head: function (userHandle, url, urlLength, timeOut, headers, done) {
+        var doneText = 'done';
+        var successCallback = function (request) {
+            // Return Headers
+            var headersText = request.getAllResponseHeaders();
+            NationalInstruments.Vireo.dataWriteString(headers, headersText, headersText.length);
+            NationalInstruments.Vireo.dataWriteString(done, doneText, doneText.length);
+        };
+        var errorCallback = function (request) {
+            console.log(request.statusText);
+            NationalInstruments.Vireo.dataWriteString(done, doneText, doneText.length);
+        };
+        var timeOutCallback = function (request) {
+            console.log(request.statusText);
+            NationalInstruments.Vireo.dataWriteString(done, doneText, doneText.length);
+        };
 
-        var successCallback = function(request) {
-           // Return Headers
-            console.log(request.getAllResponseHeaders());
-            
-            // Return body
-            console.log(request.responseText);
-        };
-        var errorCallback = function(request)
-        {
-            console.log(request.statusText);
-        };
-        
-        this.makeRequest('POST', url, buffer, successCallback, errorCallback);
-    },
-    
-    js_ni_httpClient_Put: function(url, buffer) {
-        var successCallback = function(request) {
-           // Return Headers
-            console.log(request.getAllResponseHeaders());
-            
-            // Return body
-            console.log(request.responseText);
-        };
-        var errorCallback = function(request)
-        {
-            console.log(request.statusText);
-        };
-        
-        this.makeRequest('PUT', url, buffer, successCallback, errorCallback);
-    },
-    
-    js_ni_httpClient_Delete: function(url) {
-         var successCallback = function(request) {
-           // Return Headers
-            console.log(request.getAllResponseHeaders());
-            
-            // Return body
-            console.log(request.responseText);
-        };
-        var errorCallback = function(request)
-        {
-            console.log(request.statusText);
-        };
-        
-        this.makeRequest('POST', url, undefined, successCallback, errorCallback);
-    },
-    
-    js_ni_httpClient_Head: function(url)
-    {
-        var successCallback = function(request) {
-           // Return Headers
-            console.log(request.getAllResponseHeaders());
-        };
-        var errorCallback = function(request)
-        {
-            console.log(request.statusText);
-        };
-        
-        this.makeRequest('HEAD', url, undefined, successCallback, errorCallback);
+        NationalInstruments.Vireo.makeRequest(userHandle, 'HEAD', Pointer_stringify(url, urlLength), timeOut, undefined, successCallback, errorCallback, timeOutCallback);
+
+        return 0;
     },
 
-    js_ni_httpClient_AddHeader: function (header, value) {
-        this.headers.addHeader(header, value);
+    js_ni_httpClient_PutBuffer: function (userHandle, url, urlLength, outputFile, outputFileLength, buffer, bufferLength, timeOut, headers, body, done) {
+        var doneText = 'done';
+        var successCallback = function (request) {
+            // Return Headers
+            var headersText = request.getAllResponseHeaders();
+            NationalInstruments.Vireo.dataWriteString(headers, headersText, headersText.length);
+
+            // Return body
+            var bodyText = request.responseText;
+            NationalInstruments.Vireo.dataWriteString(body, bodyText, bodyText.length);
+            NationalInstruments.Vireo.dataWriteString(done, doneText, doneText.length);
+            console.log("Success with Put for " + Pointer_stringify(url, urlLength));
+        };
+        var errorCallback = function (request) {
+            console.log("errorCallback was called with " + request.statusText);
+            NationalInstruments.Vireo.dataWriteString(done, doneText, doneText.length);
+        };
+        var timeOutCallback = function (request) {
+            console.log("timeOutCallback was called with " + request.statusText);
+            NationalInstruments.Vireo.dataWriteString(done, doneText, doneText.length);
+        };
+
+        NationalInstruments.Vireo.makeRequest(userHandle, 'PUT', Pointer_stringify(url, urlLength), timeOut, Pointer_stringify(buffer, bufferLength), successCallback, errorCallback, timeOutCallback);
+
+        return 0;
     },
-    
-    js_ni_httpClient_RemoveHeader : function(header) {
-        this.headers.removeHeader(header);   
+
+    js_ni_httpClient_Delete: function (userHandle, url, urlLength, outputFile, outputFileLength, timeOut, headers, body, done) {
+        var doneText = 'done';
+        var successCallback = function (request) {
+            // Return Headers
+            var headersText = request.getAllResponseHeaders();
+            NationalInstruments.Vireo.dataWriteString(headers, headersText, headersText.length);
+
+            // Return body
+            var bodyText = request.responseText;
+            NationalInstruments.Vireo.dataWriteString(body, bodyText, bodyText.length);
+            NationalInstruments.Vireo.dataWriteString(done, doneText, doneText.length);
+        };
+        var errorCallback = function (request) {
+            console.log(request.statusText);
+            NationalInstruments.Vireo.dataWriteString(done, doneText, doneText.length);
+        };
+        var timeOutCallback = function (request) {
+            console.log(request.statusText);
+            NationalInstruments.Vireo.dataWriteString(done, doneText, doneText.length);
+        };
+
+        NationalInstruments.Vireo.makeRequest(userHandle, 'DELETE', Pointer_stringify(url, urlLength), timeOut, undefined, successCallback, errorCallback, timeOutCallback);
+
+        return 0;
     },
-    
-    js_ni_httpClient_GetHeader : function(header) {
-        var headerValue = this.headers.getHeader(header);
-        console.log(headerValue);
-    },
-    
-    js_ni_httpClient_HeaderExists : function(header) {
-        var headerExists = this.headers.headerExists(header);
-        var headerValue = this.headers.getHeader(header);
-        
-        console.log(headerExists);
-        console.log(headerValue);
-    },
-    
-    js_ni_httpClient_ListHeaders : function() {
-        var allHeaders = this.headers.getHeaders();
-        var outputString = '';
-        for (var key in allHeaders) {
-            outputString += key + ': ' + allHeaders[key];
-            outputString += '\r\n';
-        }
-        
-        console.log(outputString);
+
+    js_ni_httpClient_PostBuffer: function (userHandle, url, urlLength, outputFile, outputFileLength, buffer, bufferLength, timeOut, headers, body, done) {
+        var doneText = 'done';
+        var successCallback = function (request) {
+            // Return Headers
+            var headersText = request.getAllResponseHeaders();
+            NationalInstruments.Vireo.dataWriteString(headers, headersText, headersText.length);
+
+            // Return body
+            var bodyText = request.responseText;
+            NationalInstruments.Vireo.dataWriteString(body, bodyText, bodyText.length);
+            NationalInstruments.Vireo.dataWriteString(done, doneText, doneText.length);
+        };
+        var errorCallback = function (request) {
+            console.log(request.statusText);
+            NationalInstruments.Vireo.dataWriteString(done, doneText, doneText.length);
+        };
+        var timeOutCallback = function (request) {
+            console.log(request.statusText);
+            NationalInstruments.Vireo.dataWriteString(done, doneText, doneText.length);
+        };
+
+        NationalInstruments.Vireo.makeRequest(userHandle, 'POST', Pointer_stringify(url, urlLength), timeOut, Pointer_stringify(buffer, bufferLength), successCallback, errorCallback, timeOutCallback);
+
+        return 0;
     }
-        
 };
+
+mergeInto(LibraryManager.library, HttpClient);
