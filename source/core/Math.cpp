28f7f17e3bda56ffc0b1cf575cc239b714f54cd0
Paul Austin <paul@ni.com>
2015-07-15 22:43:53 -0500

    npm: built a version of vireo.js, fixes to package.son.

--- source/core/Math.cpp
+++ source/core/Math.cpp
@@ -18,7 +18,7 @@ SDG
 #include "Timestamp.h"      // For seeding random numbers
 #include "TypeDefiner.h"
 
-// With non-unique names(AKA overlodaed) only the C entlry points are appended.
+// With non-unique names(AKA overlodaed) only the C entry points are appended.
 #define DEFINE_VIREO_FUNCTION_TYPED(_root_, _type_, _proto_)  DEFINE_VIREO_FUNCTION_CUSTOM(_root_, _root_##_type_, _proto_)
 #define DEFINE_VIREO_FUNCTION_2TYPED(_root_, _type1_, _type2_, _proto_)  DEFINE_VIREO_FUNCTION_CUSTOM(_root_, _type1_##_root_##_type2_, _proto_)
 
