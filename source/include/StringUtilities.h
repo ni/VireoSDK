6a90f47226b03de9accd1b25d52a95693cf4f04b
Paul Austin <paul@ni.com>
2015-08-29 08:47:34 -0500

    Strings:	Added some comments. Xcode project setting

--- source/include/StringUtilities.h
+++ source/include/StringUtilities.h
@@ -298,6 +298,7 @@ public:
     IntIndex FindFirstMatch(SubString* searchString, IntIndex offset, Boolean ignoreCase);
 };
 
+//------------------------------------------------------------
 //! Macro to help with %.* formats. Example => printf("%.*s", FMT_LEN_BEGIN(arg))
 #define FMT_LEN_BEGIN(_substring_)   (int)(_substring_)->Length(), (_substring_)->Begin()
 
@@ -331,24 +332,32 @@ class ComapreSubString
 };
 
 //------------------------------------------------------------
-//! A null terminated copy of a SubString. Used for calling OS APIs
-
+//! A class for makking temporary null terminated strings for calling OS APIs
 #define kTempCStringLength 255
  
 class TempStackCString : public FixedCArray<Utf8Char, kTempCStringLength>
 {
 public:
+    //! Construct a empty string.
     TempStackCString() {}
+
+    //! Construct a null terminated from an existing SubString.
     TempStackCString(SubString* string) : FixedCArray(string) {}
+    
+    //! Construct a null terminated from rwa block of UTF-8 characters.
     TempStackCString(Utf8Char* begin, Int32 length) : FixedCArray((Utf8Char*)begin, length) {}
     
+    //! Append a SubString.
     Boolean Append(SubString* string)
     {
         return FixedCArray::Append(string->Begin(), (size_t)string->Length());
     }
-    // Append a Null Terminated String
+    
+    //! Append a null terminated String.
     Boolean AppendCStr(ConstCStr cstr) { return FixedCArray::Append((Utf8Char*)cstr, (IntIndex)strlen(cstr)); }
 
+    //! Get the standard char* pointer to the null terminated string.
+    //! The pointer is only valid during the scope of the TempStackCString instance
     char* BeginCStr()
     {
         return (char*) _buffer;
