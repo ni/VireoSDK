295b70d4e424b335e20cd84cde534108b24218a7
Craig Smith <spathiwa@gmail.com>
2016-07-13 14:23:30 -0500

    Tweak patchVireo.js to work on optimized/no-spaces emcc output.

--- make-it/patchVireo.py
+++ make-it/patchVireo.py
@@ -18,11 +18,11 @@ except:
 destination = open(destFile, "w")
 source= open(origFile, "r")
 
-regex1 = re.compile("if\s* \(!u0\)\s*return str;", 0)
-regex2 = re.compile("if\s*\(val\s*!=\s*0\)\s*tty\.output\.push\(val\);", 0)
+regex1 = re.compile("if\s*\(!u0\)\s*return str;", 0)
+regex2 = re.compile("if\s*\(val\s*!=\s*0\)\s*tty\.output\.push\(val\)", 0)
 for line in source:
     line = regex1.sub("if(idx>u8Array.length)return str; /*bcs-don't stop at 0, allow embedded nulls*/", line)
-    line = regex2.sub("tty.output.push(val);/*bcs-removed if(val!=0)*/", line)
+    line = regex2.sub("tty.output.push(val)/*bcs-removed if(val!=0)*/", line)
     destination.write(line)
 source.close()
 destination.close()
