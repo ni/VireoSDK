b2a7fd049565cb51a87034274e371b3cd1b43980
Craig Smith <spathiwa@gmail.com>
2016-11-03 14:38:34 -0500

    Add back IsPrintable poly

--- source/core/GenericFunctions.cpp
+++ source/core/GenericFunctions.cpp
@@ -2157,9 +2157,7 @@ DEFINE_VIREO_BEGIN(Generics)
     DEFINE_VIREO_GENERIC(IsDecimalDigit, "GenericUnOp", EmitGenericUnOpInstruction);
     DEFINE_VIREO_GENERIC(IsHexDigit, "GenericUnOp", EmitGenericUnOpInstruction);
     DEFINE_VIREO_GENERIC(IsOctalDigit, "GenericUnOp", EmitGenericUnOpInstruction);
-#if 0
     DEFINE_VIREO_GENERIC(IsPrintable, "GenericUnOp", EmitGenericUnOpInstruction);
-#endif
     DEFINE_VIREO_GENERIC(IsWhiteSpace, "GenericUnOp", EmitGenericUnOpInstruction);
     DEFINE_VIREO_GENERIC(Add, "GenericBinOp", EmitGenericBinOpInstruction);
     DEFINE_VIREO_GENERIC(Sub, "GenericBinOp", EmitGenericBinOpInstruction);
