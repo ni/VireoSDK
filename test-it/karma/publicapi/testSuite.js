a9a1b05be06da810c53984aebd43b73dc48aea2b
rajsite <rajsite@users.noreply.github.com>
2016-12-14 18:32:17 -0600

    Clean-up Test Runner for HTTP Test Reuse

--- test-it/karma/publicapi/testSuite.js
+++ test-it/karma/publicapi/testSuite.js
@@ -1,61 +1,19 @@
 describe('can run test suite file', function () {
     'use strict';
 
-    var viaFiles = {};
-    var vtrFiles = {};
-    // Sharing Vireo instances across tests make them run soooo much faster
+    // Reference aliases
     var Vireo = window.NationalInstruments.Vireo.Vireo;
+    var vireoRunner = window.testHelpers.vireoRunner;
+
+    // Sharing Vireo instances across tests make them run soooo much faster
     var vireo = new Vireo();
+    var viaFiles = vireoRunner.matchNamesFromPaths(/test-it\/(.*)\.via$/);
+    var vtrFiles = vireoRunner.matchNamesFromPaths(/test-it\/results\/(.*)\.vtr$/);
 
-    Object.keys(window.__karma__.files).forEach(function (file) {
-        var viaFileParts = file.match(/test-it\/(.*)\.via$/);
-        if (viaFileParts !== null) {
-            viaFiles[viaFileParts[1]] = file;
-        }
+    var focusTests = vireoRunner.stringArrayToObjectMap([
+    ]);
 
-        var vtrFileParts = file.match(/test-it\/results\/(.*)\.vtr$/);
-        if (vtrFileParts !== null) {
-            vtrFiles[vtrFileParts[1]] = file;
-        }
-    });
-
-    Object.keys(viaFiles).forEach(function (viaFileName) {
-        if (vtrFiles[viaFileName] === undefined) {
-            throw new Error('Missing vtr file for test:', viaFileName);
-        }
-    });
-
-    var normalizeLineEndings = function (multiLineString) {
-        return multiLineString.replace(/\r\n/gm, '\n');
-    };
-
-    var removeInlineComments = function (multiLineString) {
-        return multiLineString.replace(/^\/\/.*\n/gm, '');
-    };
-
-    var createTestForVia = function (viaAbsolutePath, vtrAbsolutePath) {
-        return function () {
-            var viaText = window.testHelpers.fixtures.loadAbsoluteUrl(viaAbsolutePath);
-            var rawVtrText = window.testHelpers.fixtures.loadAbsoluteUrl(vtrAbsolutePath);
-            var rawResults = '';
-            vireo.eggShell.reboot();
-            vireo.eggShell.setPrintFunction(function (text) {
-                rawResults += text + '\n';
-            });
-            vireo.eggShell.loadVia(viaText);
-            vireo.eggShell.executeSlices(1000000);
-
-            var normalizedResults = normalizeLineEndings(rawResults);
-            var normalizedVtrText = normalizeLineEndings(rawVtrText);
-            var vtrTextNoComments = removeInlineComments(normalizedVtrText);
-            expect(JSON.stringify(normalizedResults)).toBe(JSON.stringify(vtrTextNoComments));
-        };
-    };
-
-    var focusTests = [
-    ];
-
-    var disabledTests = [
+    var disabledTests = vireoRunner.stringArrayToObjectMap([
         'BadArgumentToVarArgInstruction',
         'BadComment',
         'ClumpTriggerWait',
@@ -86,26 +44,19 @@ describe('can run test suite file', function () {
         'TimingTest1',
         'TooManyArguments',
         'Viaduino'
-    ];
-
-    var addNameToObject = function (obj, testName) {
-        obj[testName] = true;
-        return obj;
-    };
-
-    var disabledTestsObj = disabledTests.reduce(addNameToObject, {});
-    var focusTestsObj = focusTests.reduce(addNameToObject, {});
+    ]);
 
     Object.keys(viaFiles).forEach(function (testName) {
         var viaAbsolutePath = viaFiles[testName];
         var vtrAbsolutePath = vtrFiles[testName];
+        var test = vireoRunner.createVTRTest(vireo, viaAbsolutePath, vtrAbsolutePath);
 
-        if (focusTestsObj[testName] === true) {
-            fit(testName, createTestForVia(viaAbsolutePath, vtrAbsolutePath));
-        } else if (disabledTestsObj[testName] === true) {
-            xit(testName, createTestForVia(viaAbsolutePath, vtrAbsolutePath));
+        if (focusTests[testName] === true) {
+            fit(testName, test);
+        } else if (disabledTests[testName] === true) {
+            xit(testName, test);
         } else {
-            it(testName, createTestForVia(viaAbsolutePath, vtrAbsolutePath));
+            it(testName, test);
         }
     });
 });
