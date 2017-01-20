327203ea82607e9f57b1deacbbf109ad9cb36fe3
rajsite <rajsite@users.noreply.github.com>
2017-01-20 14:07:12 -0600

    Re-enable all parts of tests against http api

--- karma.shared.js
+++ karma.shared.js
@@ -61,7 +61,7 @@
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['PhantomJS'],
+        browsers: [],
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
--- test-it/karma/http/httpget.js
+++ test-it/karma/http/httpget.js
@@ -14,6 +14,7 @@ describe('Performing a GET test', function () {
     // var vireo = new Vireo();
 
     // TODO mraj using the same vireo instance causes an abort when one http call results in a none 200 response code
+    // See https://github.com/ni/VireoSDK/issues/163
     var vireo;
 
     beforeEach(function () {
@@ -36,10 +37,9 @@ describe('Performing a GET test', function () {
             expect(viPathParser('handle')).toBe(0);
             expect(viPathParser('headers')).toBeDefined();
             expect(viPathParser('body')).toBeDefined();
-            // TODO mraj enable when have correct error output
-            // expect(viPathParser('error.status')).toBe(false);
-            // expect(viPathParser('error.code')).toBe(0);
-            // expect(viPathParser('error.source')).toBe('');
+            expect(viPathParser('error.status')).toBe(false);
+            expect(viPathParser('error.code')).toBe(0);
+            expect(viPathParser('error.source')).toBe('');
             done();
         });
     });
@@ -76,52 +76,15 @@ describe('Performing a GET test', function () {
             expect(requestUrl.pathname).toBe('/get');
 
             // error
-            // TODO mraj enable when have correct error output
-            // expect(viPathParser('error.status')).toBe(false);
-            // expect(viPathParser('error.code')).toBe(0);
-            // expect(viPathParser('error.source')).toBe('');
+            expect(viPathParser('error.status')).toBe(false);
+            expect(viPathParser('error.code')).toBe(0);
+            expect(viPathParser('error.source')).toBe('');
 
             done();
         });
     });
 
-    it('validating a 404 response', function (done) {
-        var viaPath = fixtures.convertToAbsoluteFromFixturesDir('http/Get.via');
-
-        var runSlicesAsync = vireoRunner.rebootAndLoadVia(vireo, viaPath);
-        var viPathParser = vireoRunner.createVIPathParser(vireo, 'MyVI');
-        var viPathWriter = vireoRunner.createVIPathWriter(vireo, 'MyVI');
-
-        var url = httpBinHelpers.convertToAbsoluteUrl('status/404');
-        viPathWriter('url', url);
-
-        runSlicesAsync(function (rawPrint, rawPrintError) {
-            expect(rawPrint).toBe('');
-            expect(rawPrintError).toBe('');
-
-            // handle
-            expect(viPathParser('handle')).toBe(0);
-
-            // header
-            // TODO mraj Phantomjs is treating an empty 404 as an error (http error handler is triggered instead of load)
-            // var responseHeader = httpParser.parseResponseHeader(viPathParser('headers'));
-            // expect(responseHeader.httpVersion).toBe('HTTP/1.1');
-            // expect(responseHeader.statusCode).toBe(404);
-            // expect(responseHeader.reasonPhrase).toBe('NOT FOUND');
-            // expect(Object.keys(responseHeader.headers).length).toBeGreaterThan(0);
-
-            // body
-            expect(viPathParser('body')).toBe('');
-
-            // error
-            // TODO mraj enable when have correct error output
-            // expect(viPathParser('error.status')).toBe(false);
-            // expect(viPathParser('error.code')).toBe(0);
-            // expect(viPathParser('error.source')).toBe('');
-
-            done();
-        });
-    });
+    // NOTE: Validation of a 404 response code was moved to a non-PhantomJS test
 
     it('validating an unusual response code', function (done) {
         var viaPath = fixtures.convertToAbsoluteFromFixturesDir('http/Get.via');
@@ -141,21 +104,19 @@ describe('Performing a GET test', function () {
             expect(viPathParser('handle')).toBe(0);
 
             // header
-            // TODO mraj enable when header ouput valid
-            // var responseHeader = httpParser.parseResponseHeader(viPathParser('headers'));
-            // expect(responseHeader.httpVersion).toBe('HTTP/1.0');
-            // expect(responseHeader.statusCode).toBe(418);
-            // expect(responseHeader.reasonPhrase).toBe('I\'M A TEAPOT');
-            // expect(Object.keys(responseHeader.headers).length).toBeGreaterThan(0);
+            var responseHeader = httpParser.parseResponseHeader(viPathParser('headers'));
+            expect(responseHeader.httpVersion).toBe('HTTP/1.1');
+            expect(responseHeader.statusCode).toBe(418);
+            expect(responseHeader.reasonPhrase).toBe('I\'M A TEAPOT');
+            expect(Object.keys(responseHeader.headers).length).toBeGreaterThan(0);
 
             // body
             expect(viPathParser('body')).not.toBe('');
 
             // error
-            // TODO mraj enable when have correct error output
-            // expect(viPathParser('error.status')).toBe(false);
-            // expect(viPathParser('error.code')).toBe(0);
-            // expect(viPathParser('error.source')).toBe('');
+            expect(viPathParser('error.status')).toBe(false);
+            expect(viPathParser('error.code')).toBe(0);
+            expect(viPathParser('error.source')).toBe('');
 
             done();
         });
