adecae5264a09df76356f47b93620df79af90688
Craig Smith <spathiwa@gmail.com>
2017-03-15 15:51:00 -0500

    Remove unused js module functions for tz in favor of em library support

--- source/core/library_coreHelpers.js
+++ source/core/library_coreHelpers.js
@@ -2,14 +2,6 @@
 (function () {
     'use strict';
     var coreHelpersCAPI = {
-        jsTimestampGetTimeZoneAbbr: function () {
-            return Module.coreHelpers.jsTimestampGetTimeZoneAbbr.apply(undefined, arguments);
-        },
-
-        jsTimestampGetTimeZoneOffset: function () {
-            return Module.coreHelpers.jsTimestampGetTimeZoneOffset.apply(undefined, arguments);
-        },
-
         jsExecutionContextFPSync: function () {
             Module.coreHelpers.jsExecutionContextFPSync.apply(undefined, arguments);
         },
