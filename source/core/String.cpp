f7bcfce8dbab1dce6d2b7556acaa77ef8d066345
rajsite <rajsite@users.noreply.github.com>
2017-07-05 14:35:49 -0500

    Change constants to ASCII representation

--- source/core/String.cpp
+++ source/core/String.cpp
@@ -126,17 +126,17 @@ void String::AppendEscapeEncoded(const Utf8Char* source, IntIndex len)
             *ptr--  = '"'; *ptr-- = '\\';
             break;
         case '\x00': case '\x01': case '\x02': case '\x03': case '\x04': case '\x05': case '\x06': case '\x07':
-            *ptr-- = c + 48; *ptr-- = '0'; *ptr-- = '0'; *ptr-- = '0'; *ptr-- = 'u'; *ptr-- = '\\';
+            *ptr-- = c + '0'; *ptr-- = '0'; *ptr-- = '0'; *ptr-- = '0'; *ptr-- = 'u'; *ptr-- = '\\';
             break;
         case '\x0B':
         case '\x0E': case '\x0F':
-            *ptr-- = c - 10 + 65; *ptr-- = '0'; *ptr-- = '0'; *ptr-- = '0'; *ptr-- = 'u'; *ptr-- = '\\';
+            *ptr-- = c - '\x0A' + 'A'; *ptr-- = '0'; *ptr-- = '0'; *ptr-- = '0'; *ptr-- = 'u'; *ptr-- = '\\';
             break;
         case '\x10': case '\x11': case '\x12': case '\x13': case '\x14': case '\x15': case '\x16': case '\x17': case '\x18': case '\x19':
-            *ptr-- = c -16 + 48; *ptr-- = '1'; *ptr-- = '0'; *ptr-- = '0'; *ptr-- = 'u'; *ptr-- = '\\';
+            *ptr-- = c - '\x10' + '0'; *ptr-- = '1'; *ptr-- = '0'; *ptr-- = '0'; *ptr-- = 'u'; *ptr-- = '\\';
             break;
         case '\x1A': case '\x1B': case '\x1C': case '\x1D': case '\x1E': case '\x1F':
-            *ptr-- = c - 26 + 65; *ptr-- = '1'; *ptr-- = '0'; *ptr-- = '0'; *ptr-- = 'u'; *ptr-- = '\\';
+            *ptr-- = c - '\x1A' + 'A'; *ptr-- = '1'; *ptr-- = '0'; *ptr-- = '0'; *ptr-- = 'u'; *ptr-- = '\\';
             break;
         default:
             *ptr-- = c;
