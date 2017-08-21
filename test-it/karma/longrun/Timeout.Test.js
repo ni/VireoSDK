86a335b44e34fd7d07ce7283d580c48de8e8eeb6
rajsite <rajsite@users.noreply.github.com>
2017-08-21 15:51:49 -0500

    Skip CORS tests on IE11

--- test-it/karma/longrun/Timeout.Test.js
+++ test-it/karma/longrun/Timeout.Test.js
@@ -1,4 +1,4 @@
-describe('Timeout test suite #Slow', function () {
+describe('Timeout test suite #FailsPhantomJS #Slow', function () {
     'use strict';
     // Reference aliases
     var Vireo = window.NationalInstruments.Vireo.Vireo;
