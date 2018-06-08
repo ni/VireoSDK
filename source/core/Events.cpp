214a9bc3d04ea05002661d158318af3fe86b8068
Craig Smith <spathiwa@gmail.com>
2018-06-08 13:51:40 -0500

    Tweak whitespace.

--- source/core/Events.cpp
+++ source/core/Events.cpp
@@ -1012,7 +1012,7 @@ VIREO_FUNCTION_SIGNATURE3(IsNERefnum, RefNumVal, RefNumVal, Boolean);
 DEFINE_VIREO_BEGIN(Events)
 
     // EventSpec used for event structure configuration
-    DEFINE_VIREO_TYPE(EventSpec, "c(e(UInt32 eventSource)e(UInt32 eventType)e(UInt32 controlUID)e(UInt32 dynIndex)");
+    DEFINE_VIREO_TYPE(EventSpec, "c(e(UInt32 eventSource) e(UInt32 eventType) e(UInt32 controlUID) e(UInt32 dynIndex)");
 
     // User Events
     DEFINE_VIREO_TYPE(UserEventRefNum, "refnum($0)")
