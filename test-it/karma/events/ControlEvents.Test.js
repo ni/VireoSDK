f2c2361f325f4f5d3fbf1f3e67ce2bc0632207f1
Jared Segal <segaljared@gmail.com>
2018-07-18 09:01:46 -0500

    Changing the occurEvent method to have a typeVisitor parameter. Updating tests with a visitor and adding a test to verify values are passed.

--- test-it/karma/events/ControlEvents.Test.js
+++ test-it/karma/events/ControlEvents.Test.js
@@ -9,16 +9,38 @@ describe('ValueChanged event tests', function () {
 
     var valueChangedEventRegisterAndUnregister = fixtures.convertToAbsoluteFromFixturesDir('events/ValueChangeEventRegistration.via');
     var updateBooleanOnValueChangeEvent = fixtures.convertToAbsoluteFromFixturesDir('events/ValueChangeStaticControlEvent.via');
+    var updateNumericOnValueChangeEvent = fixtures.convertToAbsoluteFromFixturesDir('events/ValueChangeNumericStaticControlEvent.via');
     var updateMultipleEventStructuresOnValueChange = fixtures.convertToAbsoluteFromFixturesDir('events/ValueChangeStaticControlEventWithMultipleRegistrations.via');
 
-    var getEventDataValueRef = function (viName) {
-        return vireo.eggShell.findValueRef(viName, 'valueChangedEventDataBool');
+    var getEventDataValueRef = function (viName, controlName) {
+        return vireo.eggShell.findValueRef(viName, 'valueChangedEventData' + controlName);
     };
+
+    var typeVisitor = (function () {
+        return {
+            visitCluster: function (valueRef, data) {
+                var that = this;
+                var valueRefObject = vireo.eggShell.readValueRefObject(valueRef);
+
+                Object.keys(valueRefObject).forEach(function (name) {
+                    vireo.eggShell.reflectOnValueRef(that, valueRefObject[name], data[name]);
+                });
+            },
+            visitBoolean: function (valueRef, data) {
+                vireo.eggShell.writeDouble(valueRef, data ? 1 : 0);
+            },
+            visitInt32: function (valueRef, data) {
+                var dataNum = parseFloat(data);
+                vireo.eggShell.writeDouble(valueRef, dataNum);
+            }
+        };
+    }());
 
     beforeAll(function (done) {
         fixtures.preloadAbsoluteUrls([
             valueChangedEventRegisterAndUnregister,
             updateBooleanOnValueChangeEvent,
+            updateNumericOnValueChangeEvent,
             updateMultipleEventStructuresOnValueChange
         ], done);
     });
@@ -30,9 +52,6 @@ describe('ValueChanged event tests', function () {
         });
         vireo.eventHelpers.setUnRegisterForControlEventsFunction(function () {
             // no-op
-        });
-        vireo.eventHelpers.setWriteEventDataFunction(function (valueRef, eventData) {
-            vireo.eggShell.writeJSON(valueRef, JSON.stringify(eventData));
         });
     });
 
@@ -71,12 +90,12 @@ describe('ValueChanged event tests', function () {
         var viPathParser = vireoRunner.createVIPathParser(vireo, 'UpdateBooleanOnValueChangeEvent');
 
         setTimeout(function () {
-            const valueRef = getEventDataValueRef('UpdateBooleanOnValueChangeEvent');
+            const valueRef = getEventDataValueRef('UpdateBooleanOnValueChangeEvent', 'Bool');
             const data = {
                 OldValue: false,
                 NewValue: true
             };
-            vireo.eventHelpers.occurEvent(1, 18, 2, valueRef, data);
+            vireo.eventHelpers.occurEvent(1, 18, 2, typeVisitor, valueRef, data);
         }, 20);
 
         runSlicesAsync(function (rawPrint, rawPrintError) {
@@ -93,12 +112,12 @@ describe('ValueChanged event tests', function () {
 
         var unregisteredControlId = 19;
         setTimeout(function () {
-            const valueRef = getEventDataValueRef('UpdateBooleanOnValueChangeEvent');
+            const valueRef = getEventDataValueRef('UpdateBooleanOnValueChangeEvent', 'Bool');
             const data = {
                 OldValue: false,
                 NewValue: true
             };
-            vireo.eventHelpers.occurEvent(1, unregisteredControlId, 2, valueRef, data);
+            vireo.eventHelpers.occurEvent(1, unregisteredControlId, 2, typeVisitor, valueRef, data);
         }, 20);
 
         runSlicesAsync(function (rawPrint, rawPrintError) {
@@ -126,18 +145,43 @@ describe('ValueChanged event tests', function () {
         var viPathParser = vireoRunner.createVIPathParser(vireo, 'MultipleEventStructuresListeningToSameControl');
 
         setTimeout(function () {
-            const valueRef = getEventDataValueRef('MultipleEventStructuresListeningToSameControl');
+            const valueRef = getEventDataValueRef('MultipleEventStructuresListeningToSameControl', 'Bool');
             const data = {
                 OldValue: false,
                 NewValue: true
             };
-            vireo.eventHelpers.occurEvent(1, 18, 2, valueRef, data);
+            vireo.eventHelpers.occurEvent(1, 18, 2, typeVisitor, valueRef, data);
         }, 20);
 
         runSlicesAsync(function (rawPrint, rawPrintError) {
             expect(rawPrintError).toBeEmptyString();
             expect(viPathParser('eventStructure1Notified')).toEqual(true);
             expect(viPathParser('eventStructure2Notified')).toEqual(true);
+            done();
+        });
+    });
+
+    it('Verify event occurrence passes correct old and new value', function (done) {
+        var runSlicesAsync = vireoRunner.rebootAndLoadVia(vireo, updateNumericOnValueChangeEvent);
+        var viPathParser = vireoRunner.createVIPathParser(vireo, 'UpdateNumericOnValueChangeEvent');
+        var oldValue = 12;
+        var newValue = 13;
+
+        setTimeout(function () {
+            const valueRef = getEventDataValueRef('UpdateNumericOnValueChangeEvent', 'Numeric');
+            const data = {
+                OldValue: oldValue,
+                NewValue: newValue
+            };
+            vireo.eventHelpers.occurEvent(1, 18, 2, typeVisitor, valueRef, data);
+        }, 20);
+
+        runSlicesAsync(function (rawPrint, rawPrintError) {
+            expect(rawPrintError).toBeEmptyString();
+            expect(viPathParser('eventOccurred')).toEqual(true);
+            expect(viPathParser('oldValueInEvent')).toEqual(oldValue);
+            expect(viPathParser('newValueInEvent')).toEqual(newValue);
+            expect(viPathParser('eventTimedOut')).toEqual(false);
             done();
         });
     });
