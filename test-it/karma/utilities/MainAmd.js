85500499c99ffb5536abb36e16f6ae0d972bfe52
rajsite <rajsite@users.noreply.github.com>
2018-09-01 11:40:22 -0500

    Update to eslint 5
    Helps to remove even more unneeded JS dependencies

--- test-it/karma/utilities/MainAmd.js
+++ test-it/karma/utilities/MainAmd.js
@@ -4,7 +4,7 @@
     var tests = [];
     for (var file in window.__karma__.files) {
         if (window.__karma__.files.hasOwnProperty(file)) {
-            if (/test\.js$/.test(file)) {
+            if ((/test\.js$/).test(file)) {
                 tests.push(file);
             }
         }
