c95cf1a08ac7ec7e13ddd21ee65f4ce6ebde9b18
rajsite <rajsite@users.noreply.github.com>
2018-09-17 17:14:04 -0500

    Minified build and test

--- karma.debug.conf.js
+++ karma.debug.conf.js
@@ -7,7 +7,7 @@
         var getSharedConfig = require('./karma.shared.js');
         var sharedConfig = getSharedConfig(config);
         sharedConfig.files = [
-            'dist/wasm32-unknown-emscripten/release/vireo.js'
+            'dist/wasm32-unknown-emscripten/debug/vireo.js'
         ].concat(sharedConfig.files);
 
         config.set(sharedConfig);
--- karma.shared.js
+++ karma.shared.js
@@ -3,104 +3,123 @@
 
     process.env.CHROME_BIN = require('puppeteer').executablePath();
 
-    module.exports = {
-        // base path that will be used to resolve all patterns (eg. files, exclude)
-        basePath: '',
+    module.exports = function (config) {
+        return {
+            // base path that will be used to resolve all patterns (eg. files, exclude)
+            basePath: '',
 
-        // frameworks to use
-        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-        frameworks: ['jasmine', 'jasmine-spec-tags'],
+            // frameworks to use
+            // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+            frameworks: ['jasmine', 'jasmine-spec-tags'],
 
-        // list of files / patterns to load in the browser
-        files: [
-            // Polyfills
-            // None
+            // list of files / patterns to load in the browser
+            files: [
+                // Polyfills
+                // None
 
-            // Test infrastructure
-            'node_modules/jasmine-expect/dist/jasmine-matchers.js',
-            'node_modules/diff/dist/diff.js',
+                // Test infrastructure
+                'node_modules/jasmine-expect/dist/jasmine-matchers.js',
+                'node_modules/diff/dist/diff.js',
 
-            // Source files
-            'dist/wasm32-unknown-emscripten/release/vireo.js',
-            {
-                pattern: 'dist/wasm32-unknown-emscripten/release/vireo.core.wasm',
-                included: false
+                // Source files
+                {
+                    pattern: 'dist/wasm32-unknown-emscripten/*/vireo.core.wasm',
+                    included: false
+                },
+
+                // Test specs and assets
+                'test-it/karma/utilities/ExtendJasmineTimeout.js',
+                'test-it/karma/utilities/TestHelpers.*.js',
+                {
+                    pattern: 'test-it/ViaTests/*.via',
+                    included: false
+                },
+                {
+                    pattern: 'test-it/ExpectedResults/*.vtr',
+                    included: false
+                },
+                {
+                    pattern: 'test-it/karma/fixtures/**/*.*',
+                    included: false
+                },
+                {
+                    pattern: 'test-it/testList.json',
+                    included: false
+                },
+
+                // Test specs
+                'test-it/karma/helloworld/*.Test.js',
+                'test-it/karma/publicapi/*.Test.js',
+                'test-it/karma/vtrsuite/*.Test.js',
+                'test-it/karma/http/*.Test.js',
+                'test-it/karma/javascriptinvoke/*.Test.js',
+                'test-it/karma/propertynode/*.Test.js',
+                'test-it/karma/events/*.Test.js',
+                'test-it/karma/static/*.Test.js'
+            ],
+
+            // list of files to exclude
+            exclude: [
+            ],
+
+            // preprocess matching files before serving them to the browser
+            // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+            preprocessors: {},
+
+            // test results reporter to use
+            // possible values: 'dots', 'progress', 'verbose'
+            // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+            reporters: ['dots', 'kjhtml'],
+
+            // web server port
+            port: 9876,
+
+            // enable / disable colors in the output (reporters and logs)
+            colors: true,
+
+            // enable / disable watching file and executing tests whenever any file changes
+            autoWatch: true,
+
+            // start these browsers
+            // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+            browsers: [],
+
+            // Hostname to be used when capturing browsers.
+            // This seems to reduce intermittent hangs on Windows 7 with legacy browsers, no longer needed.
+            hostname: '127.0.0.1',
+
+            // Aliases for launchers that provide custom settings
+            // No Sandbox mode needed to run in Travis container https://docs.travis-ci.com/user/chrome#Sandboxing
+            // Launching chrome has been timing out, some forums suggest disabling proxy settings:
+            // https://github.com/karma-runner/karma-chrome-launcher/issues/175#issuecomment-378247457
+            customLaunchers: {
+                ChromeHeadlessNoSandbox: {
+                    base: 'ChromeHeadless',
+                    flags: ['--no-sandbox', '--no-proxy-server']
+                }
             },
 
-            // Test specs and assets
-            'test-it/karma/utilities/TestHelpers.*.js',
-            {
-                pattern: 'test-it/ViaTests/*.via',
-                included: false
-            },
-            {
-                pattern: 'test-it/ExpectedResults/*.vtr',
-                included: false
-            },
-            {
-                pattern: 'test-it/karma/fixtures/**/*.*',
-                included: false
-            },
-            {
-                pattern: 'test-it/testList.json',
-                included: false
-            }
-        ],
+            // Continuous Integration mode
+            // if true, Karma captures browsers, runs the tests and exits
+            singleRun: true,
 
-        // list of files to exclude
-        exclude: [
-        ],
+            // How long will Karma wait for a message from a browser before disconnecting from it (in ms).
+            // default: 10000
+            browserNoActivityTimeout: 60000,
 
-        // preprocess matching files before serving them to the browser
-        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-        preprocessors: {},
+            // Enable or disable failure on running empty test-suites. If disabled the program will return exit-code 0 and display a warning.
+            failOnEmptyTestSuite: false,
 
-        // test results reporter to use
-        // possible values: 'dots', 'progress', 'verbose'
-        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['dots', 'kjhtml'],
+            // Concurrency level
+            // how many browser should be started simultaneous
+            concurrency: 1,
 
-        // web server port
-        port: 9876,
+            // level of logging
+            // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+            logLevel: config.LOG_INFO,
 
-        // enable / disable colors in the output (reporters and logs)
-        colors: true,
-
-        // enable / disable watching file and executing tests whenever any file changes
-        autoWatch: true,
-
-        // start these browsers
-        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: [],
-
-        // Hostname to be used when capturing browsers.
-        // This seems to reduce intermittent hangs on Windows 7 with legacy browsers, no longer needed.
-        hostname: '127.0.0.1',
-
-        // Aliases for launchers that provide custom settings
-        // No Sandbox mode needed to run in Travis container https://docs.travis-ci.com/user/chrome#Sandboxing
-        // Launching chrome has been timing out, some forums suggest disabling proxy settings:
-        // https://github.com/karma-runner/karma-chrome-launcher/issues/175#issuecomment-378247457
-        customLaunchers: {
-            ChromeHeadlessNoSandbox: {
-                base: 'ChromeHeadless',
-                flags: ['--no-sandbox', '--no-proxy-server']
-            }
-        },
-
-        // Continuous Integration mode
-        // if true, Karma captures browsers, runs the tests and exits
-        singleRun: true,
-
-        // How long will Karma wait for a message from a browser before disconnecting from it (in ms).
-        // default: 10000
-        browserNoActivityTimeout: 60000,
-
-        // Enable or disable failure on running empty test-suites. If disabled the program will return exit-code 0 and display a warning.
-        failOnEmptyTestSuite: false,
-
-        // Concurrency level
-        // how many browser should be started simultaneous
-        concurrency: 1
+            // Timeout for jasmine set to 50000 in ExtendJasmineTimeout.js so warn if test time getting close
+            reportSlowerThan: 40000
+        };
     };
 }());
