409f574ac13bbd1e2ea638bf0c13d78bc33dbeea
rajsite <rajsite@users.noreply.github.com>
2018-09-21 09:33:01 -0500

    Clean-up karma files config

--- karma.conf.js
+++ karma.conf.js
@@ -6,9 +6,15 @@
     module.exports = function (config) {
         var getSharedConfig = require('./karma.shared.js');
         var sharedConfig = getSharedConfig(config);
+
         sharedConfig.files = [
-            'dist/wasm32-unknown-emscripten/release/vireo.js'
-        ].concat(sharedConfig.files);
+            ...sharedConfig.filesPolyfills,
+            ...sharedConfig.filesInfrastructure,
+            'dist/wasm32-unknown-emscripten/release/vireo.js',
+            ...sharedConfig.filesSource,
+            ...sharedConfig.filesFixtures,
+            ...sharedConfig.filesSpecs
+        ];
 
         config.set(sharedConfig);
     };
--- karma.debug.conf.js
+++ karma.debug.conf.js
@@ -6,9 +6,15 @@
     module.exports = function (config) {
         var getSharedConfig = require('./karma.shared.js');
         var sharedConfig = getSharedConfig(config);
+
         sharedConfig.files = [
-            'dist/wasm32-unknown-emscripten/debug/vireo.js'
-        ].concat(sharedConfig.files);
+            ...sharedConfig.filesPolyfills,
+            ...sharedConfig.filesInfrastructure,
+            'dist/wasm32-unknown-emscripten/debug/vireo.js',
+            ...sharedConfig.filesSource,
+            ...sharedConfig.filesFixtures,
+            ...sharedConfig.filesSpecs
+        ];
 
         config.set(sharedConfig);
     };
