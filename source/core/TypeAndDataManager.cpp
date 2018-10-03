9ddc498a50b376cac7d353d55a602d49d83301b4
rajsite <rajsite@users.noreply.github.com>
2018-10-03 16:14:11 -0500

    Bump debug build allocation limit

--- source/core/TypeAndDataManager.cpp
+++ source/core/TypeAndDataManager.cpp
@@ -100,7 +100,7 @@ TypeManager::TypeManager(TypeManagerRef parentTm)
     _totalAQAllocated = 0;
     _totalAllocationFailures = 0;
     _maxAllocated = 0;
-    _allocationLimit = 16777216;  // 16 meg for starters
+    _allocationLimit = ((2 * 1024) - 16) * 1024 * 1024;  // 2 GB - 16 MB (current wasm limit with some headroom)
 
     _typeList = nullptr;
     _baseTypeManager = parentTm;
