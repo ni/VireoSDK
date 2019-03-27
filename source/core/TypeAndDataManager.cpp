e7d3371002613cc9ed2f94e69e29baf2f9768dc6
Siddharth Subramanian <siddharth.subramanian@ni.com>
2019-03-27 16:44:53 -0500

    undo changes to IsA, since it was submitted separately

--- source/core/TypeAndDataManager.cpp
+++ source/core/TypeAndDataManager.cpp
@@ -739,11 +739,8 @@ Boolean TypeCommon::CompareType(TypeRef otherType)
     if (this == otherType) {
         return true;
     } else if (thisEncoding == kEncoding_Array && otherEncoding == kEncoding_Array) {
-        if (this->Rank() != otherType->Rank()) {
-            return false;
-        } else {
+        if (this->Rank() == otherType->Rank())
             return this->GetSubElement(0)->CompareType(otherType->GetSubElement(0));
-        }
     } else if (thisEncoding == kEncoding_Cluster && otherEncoding == kEncoding_Cluster) {
         SubString thisTypeName, otherTypeName;
         Boolean isThisIntrinsicClusterType = this->IsIntrinsicClusterDataType(&thisTypeName);
@@ -785,14 +782,9 @@ Boolean TypeCommon::IsA(TypeRef otherType, Boolean compatibleStructure)
         EncodingEnum thisEncoding = BitEncoding();
         EncodingEnum otherEncoding = otherType->BitEncoding();
 
-        if (thisEncoding == kEncoding_Array && otherEncoding == kEncoding_Array) {
-            if (this->Rank() != otherType->Rank()) {
-                bMatch = false;
-                return bMatch;
-            } else {
-                bMatch = this->GetSubElement(0)->IsA(otherType->GetSubElement(0), compatibleStructure);
-                return bMatch;
-            }
+        if (thisEncoding == kEncoding_Array && otherEncoding == kEncoding_Array && this->Rank() == otherType->Rank()) {
+            bMatch = this->GetSubElement(0)->IsA(otherType->GetSubElement(0), compatibleStructure);
+            return bMatch;
         } else if (thisEncoding == kEncoding_UInt || thisEncoding == kEncoding_S2CInt ||
                    thisEncoding == kEncoding_Ascii || thisEncoding == kEncoding_Unicode) {
             if (otherEncoding == kEncoding_UInt || otherEncoding == kEncoding_S2CInt ||
