394c98def925af30ad1d700e998b95f3edc4a70a
Siddharth Subramanian <siddharth.subramanian@ni.com>
2019-05-13 16:57:43 -0500

    update analogwaveform type instruction to include attributes

--- source/core/Waveform.cpp
+++ source/core/Waveform.cpp
@@ -44,7 +44,7 @@ VIREO_FUNCTION_SIGNATURE4(AnalogWaveformBuild, AnalogWaveform, Timestamp, Double
 DEFINE_VIREO_BEGIN(Waveform)
     DEFINE_VIREO_REQUIRE(IEEE754Math)
     DEFINE_VIREO_REQUIRE(Timestamp)
-    DEFINE_VIREO_TYPE(AnalogWaveform, "c( e(Timestamp t0) e(Double dt) e(a($0 $1)Y) )")
+    DEFINE_VIREO_TYPE(AnalogWaveform, "c( e(Timestamp t0) e(Double dt) e(a($0 $1)Y) e(Variant attributes) )")
     DEFINE_VIREO_TYPE(DigitalWaveform, "c(e(a(UInt8 * *) data)e(a(UInt32 *) transitions))")
     DEFINE_VIREO_FUNCTION(AnalogWaveformBuild, "p(o(AnalogWaveform) i(Timestamp) i(Double) i(Array))")
 DEFINE_VIREO_END()
