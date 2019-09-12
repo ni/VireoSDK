47a9788504c24d3af0fdcff9c0c2c8c7e03dd400
rajsite <rajsite@users.noreply.github.com>
2019-09-12 00:42:35 -0500

    Additional JSS tests

--- test-it/karma/publicapi/CheckValueUpdateNeeded.Test.js
+++ test-it/karma/publicapi/CheckValueUpdateNeeded.Test.js
@@ -26,33 +26,66 @@ describe('The Vireo testAndResetNeedsUpdateFlag api', function () {
         internalModule = undefined;
     });
 
-    it('can perform a simple fpsync', async function () {
+    it('can mark data items as update needed and clear', async function () {
         var runSlicesAsync = vireoRunner.rebootAndLoadVia(vireo, publicApiCheckValueRefUpdateNeededUrl);
 
-        const {rawPrint, rawPrintError} = await runSlicesAsync();
+        var outputParameter = vireo.eggShell.findValueRef('MyVI', 'outputParameter');
+        var inputParameter = vireo.eggShell.findValueRef('MyVI', 'inputParameter');
+        var inputOutputParameter = vireo.eggShell.findValueRef('MyVI', 'inputOutputParameter');
+        var localDataItem = vireo.eggShell.findValueRef('MyVI', 'localDataItem');
+        var local = vireo.eggShell.findValueRef('MyVI', 'local');
+        var localDataItemArray = vireo.eggShell.findValueRef('MyVI', 'localDataItemArray');
+        var localArray = vireo.eggShell.findValueRef('MyVI', 'localArray');
+        var subVIOutputParameter = vireo.eggShell.findValueRef('MySubVI', 'subVIOutputParameter');
+        var subVILocalDataItem = vireo.eggShell.findValueRef('MySubVI', 'subVILocalDataItem');
 
-        var outputValueRef = vireo.eggShell.findValueRef('MyVI', 'output');
-        var doubleValueRef = vireo.eggShell.findValueRef('MyVI', 'myDouble');
-        var otherDoubleValueRef = vireo.eggShell.findValueRef('MyVI', 'myOtherDouble');
-        expect(internalModule.eggShell.testNeedsUpdateFlagWithoutReset(outputValueRef)).toBeTrue();
-        expect(vireo.eggShell.testAndResetNeedsUpdateFlag(outputValueRef)).toBeTrue();
-        expect(vireo.eggShell.testAndResetNeedsUpdateFlag(doubleValueRef)).toBeTrue();
-        expect(vireo.eggShell.testAndResetNeedsUpdateFlag(otherDoubleValueRef)).toBeFalse();
+        // Before running expect no updates needed
+        expect(internalModule.eggShell.testNeedsUpdateFlagWithoutReset(outputParameter)).toBeFalse();
+        expect(internalModule.eggShell.testNeedsUpdateFlagWithoutReset(inputParameter)).toBeFalse();
+        expect(internalModule.eggShell.testNeedsUpdateFlagWithoutReset(inputOutputParameter)).toBeFalse();
+        expect(internalModule.eggShell.testNeedsUpdateFlagWithoutReset(localDataItem)).toBeFalse();
+        expect(internalModule.eggShell.testNeedsUpdateFlagWithoutReset(local)).toBeFalse();
+        expect(internalModule.eggShell.testNeedsUpdateFlagWithoutReset(localDataItemArray)).toBeFalse();
+        expect(internalModule.eggShell.testNeedsUpdateFlagWithoutReset(localArray)).toBeFalse();
+        expect(internalModule.eggShell.testNeedsUpdateFlagWithoutReset(subVIOutputParameter)).toBeFalse();
+        expect(internalModule.eggShell.testNeedsUpdateFlagWithoutReset(subVILocalDataItem)).toBeFalse();
 
-        var integerValueRef = vireo.eggShell.findValueRef('MySubVI', 'myInteger');
-        var myOutputValueRef = vireo.eggShell.findValueRef('MySubVI', 'myOutput');
-        expect(vireo.eggShell.testAndResetNeedsUpdateFlag(integerValueRef)).toBeFalse();
-        expect(vireo.eggShell.testAndResetNeedsUpdateFlag(myOutputValueRef)).toBeFalse();
-
-        // previous testAndResetNeedsUpdateFlag should have reset all flags to be false
-        expect(vireo.eggShell.testAndResetNeedsUpdateFlag(doubleValueRef)).toBeFalse();
-        expect(vireo.eggShell.testAndResetNeedsUpdateFlag(otherDoubleValueRef)).toBeFalse();
-        expect(vireo.eggShell.testAndResetNeedsUpdateFlag(outputValueRef)).toBeFalse();
-
-        expect(vireo.eggShell.testAndResetNeedsUpdateFlag(integerValueRef)).toBeFalse();
-        expect(vireo.eggShell.testAndResetNeedsUpdateFlag(myOutputValueRef)).toBeFalse();
-
+        // Run the VI
+        var {rawPrint, rawPrintError} = await runSlicesAsync();
         expect(rawPrint).toBeEmptyString();
         expect(rawPrintError).toBeEmptyString();
+
+        // After running expect dataItem updates needed
+        expect(internalModule.eggShell.testNeedsUpdateFlagWithoutReset(outputParameter)).toBeTrue();
+        expect(internalModule.eggShell.testNeedsUpdateFlagWithoutReset(inputParameter)).toBeFalse();
+        expect(internalModule.eggShell.testNeedsUpdateFlagWithoutReset(inputOutputParameter)).toBeTrue();
+        expect(internalModule.eggShell.testNeedsUpdateFlagWithoutReset(localDataItem)).toBeTrue();
+        expect(internalModule.eggShell.testNeedsUpdateFlagWithoutReset(local)).toBeFalse();
+        expect(internalModule.eggShell.testNeedsUpdateFlagWithoutReset(localDataItemArray)).toBeTrue();
+        expect(internalModule.eggShell.testNeedsUpdateFlagWithoutReset(localArray)).toBeFalse();
+        expect(internalModule.eggShell.testNeedsUpdateFlagWithoutReset(subVIOutputParameter)).toBeFalse();
+        expect(internalModule.eggShell.testNeedsUpdateFlagWithoutReset(subVILocalDataItem)).toBeFalse();
+
+        // Test the needs update flags
+        expect(vireo.eggShell.testAndResetNeedsUpdateFlag(outputParameter)).toBeTrue();
+        expect(vireo.eggShell.testAndResetNeedsUpdateFlag(inputParameter)).toBeFalse();
+        expect(vireo.eggShell.testAndResetNeedsUpdateFlag(inputOutputParameter)).toBeTrue();
+        expect(vireo.eggShell.testAndResetNeedsUpdateFlag(localDataItem)).toBeTrue();
+        expect(vireo.eggShell.testAndResetNeedsUpdateFlag(local)).toBeFalse();
+        expect(vireo.eggShell.testAndResetNeedsUpdateFlag(localDataItemArray)).toBeTrue();
+        expect(vireo.eggShell.testAndResetNeedsUpdateFlag(localArray)).toBeFalse();
+        expect(vireo.eggShell.testAndResetNeedsUpdateFlag(subVIOutputParameter)).toBeFalse();
+        expect(vireo.eggShell.testAndResetNeedsUpdateFlag(subVILocalDataItem)).toBeFalse();
+
+        // expect all needs update flags to be reset
+        expect(vireo.eggShell.testAndResetNeedsUpdateFlag(outputParameter)).toBeFalse();
+        expect(vireo.eggShell.testAndResetNeedsUpdateFlag(inputParameter)).toBeFalse();
+        expect(vireo.eggShell.testAndResetNeedsUpdateFlag(inputOutputParameter)).toBeFalse();
+        expect(vireo.eggShell.testAndResetNeedsUpdateFlag(localDataItem)).toBeFalse();
+        expect(vireo.eggShell.testAndResetNeedsUpdateFlag(local)).toBeFalse();
+        expect(vireo.eggShell.testAndResetNeedsUpdateFlag(localDataItemArray)).toBeFalse();
+        expect(vireo.eggShell.testAndResetNeedsUpdateFlag(localArray)).toBeFalse();
+        expect(vireo.eggShell.testAndResetNeedsUpdateFlag(subVIOutputParameter)).toBeFalse();
+        expect(vireo.eggShell.testAndResetNeedsUpdateFlag(subVILocalDataItem)).toBeFalse();
     });
 });
