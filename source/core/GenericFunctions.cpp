1342b94f8537d6476a880225161e97fc5e1a8b15
Carlos Gonzalez <cglzaguilar@users.noreply.github.com>
2020-01-23 14:49:47 -0600

    Fixed linting errors

--- source/core/GenericFunctions.cpp
+++ source/core/GenericFunctions.cpp
@@ -1744,8 +1744,7 @@ VIREO_FUNCTION_SIGNATUREV(ArrayConcatenateInternal, ArrayConcatenateInternalPara
                 minInputRank = inputRank;
             if (inputRank == outputRank) {
                 inputOuterDimSize =  arrayInput->DimensionLengths()[inputRank-1];
-                if (inputOuterDimSize == 0 && !isDestSameAsFirstInput) // Uninitialized dimension should count as 1, otherwise memory corruption will occur
-                {
+                if (inputOuterDimSize == 0 && !isDestSameAsFirstInput) {  // Uninitialized dimension should count as 1, otherwise memory corruption will occur
                     inputOuterDimSize = 1;
                 }
             } else {
